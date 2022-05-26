6508cefadd2ef03040538e4902bc122e910b726f
aresous <aresous@gmail.com>
2022-05-26 02:05:56 -0400

    readable date time

--- components/Comment.js
+++ components/Comment.js
@@ -1,18 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/Comment.module.css";
-// import formatDistance from 'date-fns/formatDistance'
-
-// function TestComponent() {
-//    const dateStr = "2021-10-26T12:24:33.433+00:00";
-//    const str = formatDistance(
-//        new Date(dateStr),
-//        new Date()
-//    );
-//    return <h1>{str}</h1>
-// }
 
 export default function Comment({ comment }) {
+  let date = new Date(comment.createdAt);
+  date.toDateString(); // 'Wed May 25 2022'
+  date.toGMTString(); // 'Wed, 25 May 2022 07:10:17 GMT'
+  date.getMonth(); // 4
   return (
     <div className={styles.container}>
       <Link href={`users/${comment.uid}`}>
@@ -20,7 +14,7 @@ export default function Comment({ comment }) {
       </Link>
       <p className={styles.content}>{comment.content}</p>
 
-      <h2>{comment.createdAt}</h2>
+      <h2>{new Date(comment.createdAt).toDateString()}</h2>
     </div>
   );
 }

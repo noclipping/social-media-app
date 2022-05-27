1513c91392834810a227a68e978a1ee601305e88
aresous <aresous@gmail.com>
2022-05-26 21:54:20 -0400

    connect to database on new production load

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,5 +1,7 @@
 import Posts from "../../models/postModel";
+import connectDB from "../auth/lib/connectDB";
 export default async function handler(req, res) {
+  connectDB();
   const posts = await Posts.find().sort({ createdAt: -1 });
   res.status(200).json({ posts });
 }

f2a0400d0ad73c1bb63e947b9038bdb8ab46d601
aresous <aresous@gmail.com>
2022-05-27 17:19:11 -0400

    comment liking fully functional

--- components/Comment.js
+++ components/Comment.js
@@ -1,8 +1,12 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Comment.module.css";
 
 export default function Comment({ comment, postId }) {
+  const { data: session } = useSession();
+  const [commentLiked, setCommentLiked] = useState(false);
   let date = new Date(comment.createdAt);
   let currentTime = new Date();
   const timeElapsed = currentTime.getTime() - date;
@@ -11,6 +15,11 @@ export default function Comment({ comment, postId }) {
   const hours = Math.round(timeElapsed / 3600000);
   const days = Math.floor(timeElapsed / (24 * 3600000));
   const years = Math.floor(timeElapsed / (24 * 3600000 * 365));
+  useEffect(() => {
+    if (comment.likedBy.includes(session?.user._id)) {
+      setCommentLiked(true);
+    }
+  }, [session]);
   function getTimeElapsed() {
     return timeElapsed < 60000
       ? seconds + (seconds < 2 ? " second" : " seconds")
@@ -23,6 +32,14 @@ export default function Comment({ comment, postId }) {
       : years + (years < 2 ? " year" : " years");
   }
   const handleLike = (e) => {
+    if (commentLiked) {
+      setCommentLiked(false);
+      comment.likes -= 1;
+    } else {
+      setCommentLiked(true);
+
+      comment.likes += 1;
+    }
     fetch(`/api/comments/like`, {
       method: "POST",
       headers: {
@@ -31,6 +48,8 @@ export default function Comment({ comment, postId }) {
       body: JSON.stringify({
         commentId: comment._id,
         postId: postId,
+        userId: session.user._id,
+        liked: commentLiked,
       }),
     })
       .then((res) => res.json())
@@ -50,7 +69,7 @@ export default function Comment({ comment, postId }) {
               handleLike(e);
             }}
           >
-            ❤️ {comment.likes}
+            {!commentLiked ? "❤" : "❤️"} {comment.likes}
           </div>
           <div className={styles.timeElapsed}>{getTimeElapsed()} ago</div>
         </div>
--- pages/api/comments/like.js
+++ pages/api/comments/like.js
@@ -5,21 +5,40 @@ export default async function handler(req, res) {
   const commentId = req.body.commentId; // this is being casted as a string, so we use mongoose
   // types obj id to convert commentID to a mongoObject for our comparison
   const postId = req.body.postId;
-
+  const userId = req.body.userId;
+  const liked = req.body.liked;
   /*
   Add a check here to see if the user liked it already, gonna need to be sent the
   user's ID making the request, and then see if his name is contained within
   the comment's likedBy array, if so, decerement the likes, otherwise, increase.
   */
+  if (!liked) {
+    await Comments.findByIdAndUpdate(commentId, {
+      $inc: { likes: 1 },
+    }).then((data) => {});
 
-  await Comments.findByIdAndUpdate(commentId, {
-    $inc: { likes: 1 },
-  }).then((data) => {});
+    await Post.findOneAndUpdate(
+      { _id: postId, "comments._id": mongoose.Types.ObjectId(commentId) },
+      {
+        $inc: { "comments.$.likes": 1 },
+        $push: { "comments.$.likedBy": userId },
+      }
+    ).then((data) => {
+      res.status(200).json(data);
+    });
+  } else {
+    await Comments.findByIdAndUpdate(commentId, {
+      $inc: { likes: -1 },
+    }).then((data) => {});
 
-  await Post.findOneAndUpdate(
-    { _id: postId, "comments._id": mongoose.Types.ObjectId(commentId) },
-    { $inc: { "comments.$.likes": 1 } }
-  ).then((data) => {
-    res.status(200).json(data);
-  });
+    await Post.findOneAndUpdate(
+      { _id: postId, "comments._id": mongoose.Types.ObjectId(commentId) },
+      {
+        $inc: { "comments.$.likes": -1 },
+        $pull: { "comments.$.likedBy": userId },
+      }
+    ).then((data) => {
+      res.status(200).json(data);
+    });
+  }
 }

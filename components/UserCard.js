c980c536ad35fffde6748050939c4b7d643ed9f4
aresous <aresous@gmail.com>
2022-06-09 02:06:12 -0400

    add friend functionality

--- components/UserCard.js
+++ components/UserCard.js
@@ -4,19 +4,54 @@ import styles from "../styles/UserCard.module.css";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { server } from "../config";
 
 export default function UserCard({ user, userFriends }) {
   const [isFriend, setIsFriend] = useState(false);
+  const [rqSent, setRqSent] = useState(false);
   const { data: session } = useSession();
   const router = useRouter();
   useEffect(() => {
-    console.log(userFriends);
+    const rqSent = user.notifications.filter(
+      (notif) => notif.userId === session?.user._id
+    );
+    setRqSent(rqSent);
+    console.log(rqSent, "rqsent");
     if (userFriends.includes(session?.user._id)) {
       setIsFriend(true);
     } else {
       setIsFriend(false);
     }
+
+    if (rqSent.length > 0) {
+      setIsFriend(true);
+      console.log("triggered");
+    }
   }, [session, user, session?.user, router]);
+  function handleAddFriend() {
+    // console.log(user._id);
+    setIsFriend(true);
+    const username = session.user.username;
+    const userId = session.user._id;
+    const recipient = user._id;
+    fetch(`${server}/api/notifications/send`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "friendRequest",
+        message: "wants to be your friend.",
+        username,
+        userId,
+        recipient,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, " api response");
+      });
+  }
   return (
     <div className={styles.parent_container}>
       <img src="https://i.stack.imgur.com/34AD2.jpg" className={styles.img} />
@@ -26,7 +61,13 @@ export default function UserCard({ user, userFriends }) {
       {isFriend ? (
         <div></div>
       ) : (
-        <button style={{ justifySelf: "end" }}>Add Friend</button>
+        <span
+          className={styles.addFriend}
+          style={{ justifySelf: "end" }}
+          onClick={handleAddFriend}
+        >
+          Add Friend
+        </span>
       )}
     </div>
   );

e989f1965956be42e142c23d98da74a88f01a8ab
aresous <aresous@gmail.com>
2022-06-15 00:42:38 -0400

    fixed multi accepts

--- pages/api/notifications/acceptFriendRequest.js
+++ pages/api/notifications/acceptFriendRequest.js
@@ -2,6 +2,15 @@ import User from "../../models/userModel";
 import mongoose from "mongoose";
 export default async function handler(req, res) {
   const body = req.body;
+
+  await User.findOneAndUpdate(
+    { _id: body.currentUser },
+    { $pull: { friends: body.senderId } }
+  ).then((resp) => {});
+  await User.findOneAndUpdate(
+    { _id: body.senderId },
+    { $pull: { friends: body.currentUser } }
+  );
   await User.findOneAndUpdate(
     { _id: body.currentUser },
     { $push: { friends: body.senderId } }
@@ -14,6 +23,13 @@ export default async function handler(req, res) {
   await User.findOneAndUpdate(
     { _id: body.currentUser },
     { $pull: { notifications: { _id: body.notificationid } } }
+  ).then((resp) => {
+    res.status(200).send(resp);
+  });
+
+  await User.findOneAndUpdate(
+    { _id: body.notificationid },
+    { $pull: { notifications: { _id: body.currentUser } } }
   ).then((resp) => {
     res.status(200).send(resp);
   });

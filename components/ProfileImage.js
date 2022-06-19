1bccb7b285bcd82dc12be92bc76809a08b610a9a
aresous <aresous@gmail.com>
2022-06-18 23:57:14 -0400

    image deletion fix

--- components/ProfileImage.js
+++ components/ProfileImage.js
@@ -7,12 +7,17 @@ import { useEffect } from "react";
 export default function ProfileImage({ changeImage }) {
   const { data: session } = useSession();
   let [imageUrl, setImageUrl] = useState();
-
+  let [imgUrlChanged, setImgUrlChanged] = useState(false);
+  const [newImgUrl, setNewImgUrl] = useState("");
   let { FileInput, openFileDialog, uploadToS3 } = useS3Upload();
 
   let handleFileChange = async (file) => {
     let { url } = await uploadToS3(file);
-
+    let deletion = session.user.image.slice(48);
+    if (imgUrlChanged) {
+      console.log("triggered imgUrlChanged");
+      deletion = newImgUrl;
+    }
     setImageUrl(url);
     fetch(`${server}/api/users/uploadImage`, {
       method: "POST",
@@ -22,7 +27,7 @@ export default function ProfileImage({ changeImage }) {
       body: JSON.stringify({
         currentUser: session.user._id,
         url: url,
-        deleteUrl: session.user.image.slice(48),
+        deleteUrl: deletion,
       }),
     })
       .then((res) => res.json())
@@ -30,6 +35,10 @@ export default function ProfileImage({ changeImage }) {
         console.log(data, "data");
       });
     changeImage(url);
+    setImgUrlChanged(true);
+    setNewImgUrl(url.slice(48));
+    console.log(setImgUrlChanged, "imgurlchanged \n");
+    console.log(newImgUrl, "newImgUrl");
   };
 
   return (

2ef2245236c699caf686af1675f56fccf4544e6a
aresous <aresous@gmail.com>
2022-06-27 02:01:34 -0400

    remove unused filed

--- pages/schemas/RegisterValidation.js
+++
@@ -1,7 +0,0 @@
-import { object, string, number, date, InferType } from "yup";
-
-export const userSchema = object({
-  username: string().min(3).max(24).required(),
-  password: string().min(3).max(50).required(),
-  email: string().email().required(),
-});

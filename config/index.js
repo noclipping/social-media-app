325dfe70f89729877c2c0f5e48c345bdc95f56eb
aresous <aresous@gmail.com>
2022-06-28 01:25:25 -0400

    prod url

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "social-media-app-murex.vercel.app";
+  : "https://social-media-app-murex.vercel.app";

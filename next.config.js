c6b2f1d69869fe0cffdcff45d1e8a41143bd7b43
aresous <aresous@gmail.com>
2022-07-03 02:29:31 -0400

    s3 perms

--- next.config.js
+++ next.config.js
@@ -20,4 +20,10 @@ module.exports = {
       },
     ];
   },
+  images: {
+    domains: [
+      `${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
+      `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
+    ],
+  },
 };
